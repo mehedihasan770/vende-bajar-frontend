e67abb8c6a43a0c71c1d2e0c71b613584746b2a4
mehedihasan770 <mehedihasantrbd@gmail.com>
2026-03-17 20:08:11 +0600

    chore: restructure directory and organize component architecture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        সব CSS ডিলিট! এখন আমার রাজত্ব। 😎
-      </h1>
-    </div>
+    <>
+      {/* main content */}
+      <main>
+          <h1>hallo Home</h1>
+      </main>
+    </>
   );
 }
