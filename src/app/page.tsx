d40d23c1ff5f58f8bd616e214373b621d301bc79
mehedihasan770 <mehedihasantrbd@gmail.com>
2026-03-17 17:52:54 +0600

    feat: init customized Next.js boilerplate with TS, Tailwind, React Compiler, and src/ directory setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        সব CSS ডিলিট! এখন আমার রাজত্ব। 😎
+      </h1>
+    </div>
+  );
+}
