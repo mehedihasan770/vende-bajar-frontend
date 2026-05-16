370973cd1251839f48df51f178ea14ea6d668660
mehedihasan770 <mehedihasantrbd@gmail.com>
2026-05-16 16:09:07 +0600

    chore: add global.d.ts to fix css import type errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
             </header>
 
             {/* main contents */}
-            <main className="max-w-[92%] md:max-w-10/12 2xl:max-w-[1500px] mx-auto">
+            <main className="max-w-[92%] md:max-w-10/12 2xl:max-w-375 mx-auto">
               {children}
             </main>
 
